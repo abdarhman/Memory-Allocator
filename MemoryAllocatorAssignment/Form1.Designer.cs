f5369e4cec9cdc60df485099ade4952afeadb5ea
abdarhman taha <abdarhman.bas93@gmail.com>
2015-06-02 21:16:32 +0200

    Final Running Version

---
+++ MemoryAllocatorAssignment/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace MemoryAllocatorAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Hole";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Holes addresses details";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Base";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Size";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Size";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(57, 261);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(246, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Processes addresses details ordered by arrival time";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Add Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(203, 26);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Enter memory details";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "View Schedule";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Location = new System.Drawing.Point(7, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(291, 390);
+            this.panel1.TabIndex = 14;
+            this.panel1.Visible = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(5, 8);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(283, 379);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(275, 353);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Schedule";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(132, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Select the scgeduling type";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(263, 21);
+            this.comboBox1.TabIndex = 17;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(6, 72);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(263, 275);
+            this.listView1.TabIndex = 16;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Memory Item";
+            this.columnHeader1.Width = 81;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Base";
+            this.columnHeader2.Width = 66;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 3;
+            this.columnHeader3.Text = "Size";
+            this.columnHeader3.Width = 72;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.DisplayIndex = 2;
+            this.columnHeader4.Text = "To";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView3);
+            this.tabPage2.Controls.Add(this.listView2);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(275, 353);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Steps review";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView3
+            // 
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView3.Location = new System.Drawing.Point(131, 162);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(138, 185);
+            this.listView3.TabIndex = 2;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Busy Areas";
+            this.columnHeader6.Width = 54;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "From";
+            this.columnHeader7.Width = 39;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "To";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView2.Location = new System.Drawing.Point(3, 162);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(124, 185);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Free Areas";
+            this.columnHeader5.Width = 54;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "From";
+            this.columnHeader9.Width = 37;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "To";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(3, 6);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(266, 150);
+            this.textBox4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 395);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MemoryAllocator";
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ MemoryAllocatorAssignment/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryAllocatorAssignment
+{
+    public partial class Form1 : Form
+    {
+        LinkedList<MemoryElement> Memory = new LinkedList<MemoryElement> ();
+        LinkedList<MemoryElement> save = new LinkedList<MemoryElement> ();
+        LinkedList<MemoryElement> Processes = new LinkedList<MemoryElement>();
+        int pi = 0;
+        int hi = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = "Choose from here !";
+            comboBox1.Items.Add("Best Fit");
+            comboBox1.Items.Add("First Fit");
+            comboBox1.Items.Add("Worst Fit");
+            label5.Text = "Hole"+hi.ToString();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MemoryElement temp = new MemoryElement("Hole"+hi.ToString(), "Hole", Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            Memory.AddLast(temp);
+            MemoryElement savetemp = new MemoryElement("Hole" + hi.ToString(), "Hole", Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            save.AddLast(savetemp);
+            hi++;
+            label5.Text = "Hole" + hi.ToString();
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MemoryElement temp = new MemoryElement("Process" + pi.ToString(), "Process",0, Convert.ToInt32(textBox3.Text));
+            Processes.AddLast(temp);
+            pi++;
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            label5.Visible = false;
+        }
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+            textBox4.Text = "";
+            IEnumerable<MemoryElement> SortedMemory;
+            if (comboBox1.Text == "Best Fit") { SortedMemory =  Memory.OrderBy( memoryElement =>memoryElement.size); textBox4.AppendText("You have chosen Best Fit option" + Environment.NewLine); }
+            else if (comboBox1.Text == "First Fit") { SortedMemory = Memory.OrderBy(memoryElement => memoryElement.Base); textBox4.AppendText("You have chosen First Fit option !" + Environment.NewLine); }
+            else { SortedMemory = Memory.OrderByDescending(memoryElement => memoryElement.size); textBox4.AppendText("You have chosen Worst Fit option !" + Environment.NewLine); }
+            List<MemoryElement> SortedMemoryList = SortedMemory.ToList();
+            //for (int k = 0; k < Processes.Count; k++)
+            foreach (MemoryElement pros in Processes)
+            {
+                //for (int i = 0; i < Memory.Count; i++)
+                foreach (MemoryElement me in SortedMemoryList)
+                {
+                    if (me.type == "Hole")
+                    {
+                        if (pros.size <= me.size)
+                        {
+                            textBox4.AppendText(pros.Name + " would be allocated at " + me.Name + Environment.NewLine);
+                            me.size = me.size - pros.size;
+                            textBox4.AppendText(me.Name + "'s new size = " + me.size.ToString() + Environment.NewLine);
+                            pros.Base = me.Base;
+                            textBox4.AppendText(pros.Name + "'s allocated base = " + pros.Base.ToString() + Environment.NewLine);
+                            me.Base = me.Base + pros.size;
+                            textBox4.AppendText(me.Name + "'s new base = " + me.Base.ToString() + Environment.NewLine);
+                            if (me.size == 0)
+                            { Memory.Remove(me); textBox4.AppendText(me.Name + " is deleted !" + Environment.NewLine); }
+                            //Memory.Add(Processes[k]);
+                            SortedMemoryList.Add(pros);
+                            break;
+                        }
+                    }
+                }
+            }
+            //Memory.Sort(CompareBases);
+            IEnumerable<MemoryElement> RenderedList = SortedMemoryList.OrderBy(memoryElement => memoryElement.Base);
+            //for (int i = 0; i < Memory.Count; i++)
+            foreach (MemoryElement fu in RenderedList)
+            {
+                ListViewItem lvi = new ListViewItem(fu.Name);
+                lvi.SubItems.Add(fu.Base.ToString());
+                lvi.SubItems.Add(fu.size.ToString());
+                lvi.SubItems.Add((fu.Base + fu.size).ToString());
+                listView1.Items.Add(lvi);
+                if (fu.type == "Hole")
+                {
+                    ListViewItem free = new ListViewItem(fu.Name);
+                    free.SubItems.Add(fu.Base.ToString());
+                    free.SubItems.Add((fu.Base + fu.size).ToString());
+                    listView2.Items.Add(free);
+                }
+                else if (fu.type == "Process")
+                {
+                    ListViewItem occupied = new ListViewItem(fu.Name);
+                    occupied.SubItems.Add(fu.Base.ToString());
+                    occupied.SubItems.Add((fu.Base + fu.size).ToString());
+                    listView3.Items.Add(occupied);
+                }
+
+            }
+            Memory.Clear();
+            foreach (MemoryElement u in save)
+            {
+                Memory.AddLast(new MemoryElement(u.Name, u.type, u.Base, u.size));
+            }
+            MessageBox.Show("Go to Review Steps tab for step by step details !");
+        }
+        private int CompareBases (MemoryElement m1, MemoryElement m2)
+        {
+            return m1.Base.CompareTo(m2.Base);
+            //if (m1.Base >= m2.Base) { return 1; }
+            //else { return 0; }
+        }
+        private int CompareSizes (MemoryElement m1, MemoryElement m2)
+        {
+            if (m1.size >= m2.size) { return 1; }
+            else { return 0; }
+        }
+        private int InverseCompareSizes(MemoryElement m1, MemoryElement m2)
+        {
+            if (m1.size >= m2.size) { return 0; }
+            else { return 1; }
+        }
+
+        
+    }
+    public class MemoryElement
+    {
+        public string Name,type;
+        public int Base, size;
+        public MemoryElement(string n, string t, int b, int s)
+        { Name = n; type = t; Base = b; size = s; }
+    }
+}
